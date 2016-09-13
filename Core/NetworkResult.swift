9ed014d1bfa84256a77f555ed601f9aaaaa39834
Carl Chen <ccf.developer@gmail.com>
2016-09-13 16:40:30 +0800

    init

---
+++ Core/NetworkResult.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkResult.swift
+//  SwiftChainingDemo
+//
+//  Created by Carl Chen on 9/13/16.
+//  Copyright © 2016 nswebfrog. All rights reserved.
+//
+
+import Foundation
---
+++ Core/UIChain.swift
@@ -0,0 +1,9 @@
+//
+//  UIChain.swift
+//  SwiftChainingDemo
+//
+//  Created by Carl Chen on 9/13/16.
+//  Copyright © 2016 nswebfrog. All rights reserved.
+//
+
+import UIKit
